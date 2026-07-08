fc7903a806d92a4024cdedfdd048eb62ca8a0eac
João Pedro Barbosa <info.joaopedrobarbosa@gmail.com>
2026-07-08 17:59:05 -0300

    tirar tudo

--- src/components/Loading/index.jsx
+++
@@ -1,9 +0,0 @@
-import StyledDiv from './style.js';
-
-export default function Loading({tema}) {
-  return (
-    <StyledDiv>
-      <div className={`${tema} aguardando`}></div>
-    </StyledDiv>
-  )
-}
--- src/components/Tag/index.jsx
+++
@@ -1,5 +0,0 @@
-import { StyledSpan } from "./style"
-
-export default function Tag({fundo, children}) {
-  return <StyledSpan className={fundo}>{children}</StyledSpan>
-}
